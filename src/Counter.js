af9e74bf12e4180cfac1b01035904de7116676a3
ASUS <Yehor Kholodiuk>
2021-12-08 13:05:11 +0200

    initial commit

---
+++ src/Counter.js
@@ -0,0 +1,17 @@
+import React from "react";
+function Counter(props){
+    const onMinus = props.onMinus;
+    const onPlus = props.onPlus;
+    const value = props.value;
+    const id = props.id;
+
+    return(
+<div>
+    <button onClick={() => onMinus(id)}> - </button>
+    {value}
+    <button onClick={()=> onPlus(id)}> + </button>
+</div>
+    )
+}
+
+export default Counter;
